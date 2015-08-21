0936c838d6d4a35770c3d59fe120fcda87d8e577
Kari Burns <kariannechristina@gmail.com>
2015-08-21 13:50:43 -0700

    server config

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ var express = require('express');
 
 var app = express();
 
-module.exports = app;
-
-app.listen(3000);
 app.set('views', __dirname+'/views');
 app.set('view engine', 'ejs');
 require('./paths/middleware.js')(app, express);
 
-var port = 3000;
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'));
+//app.listen(3000);
+
+var port = app.get('port');
 var ip = "127.0.0.1";
 console.log("Listening on http://"+ip+":"+port);
+
+module.exports = app;
